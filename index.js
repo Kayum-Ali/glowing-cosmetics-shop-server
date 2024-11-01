fcc4e6c7da3f9b4c020d684298b080930af128fc
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-11-01 17:06:35 +0600

    review get and post route added

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,  } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
+var corsOptions = {
+  origin: [ 'https://glowing-cosmetics-shop.web.app', 'http://localhost:5173'],
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-app.use(cors());
+
+app.use(cors(corsOptions));
 app.use(express.json())
 
 
@@ -25,14 +30,58 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const database = client.db("glowingCosmeticShop");
+     const productsCollection = database.collection("featuredProductsCollection");
+     const reviewCollection = database.collection("reviewCollection");
+
     app.get('/featured-products', async(req,res)=>{
-        const database = client.db("glowingCosmetic");
-         const productsCollection = database.collection("featuredProductsCollection");
+      // const database = client.db("glowingCosmeticShop");
+      // const productsCollection = database.collection("featuredProductsCollection");
         const cursor =  productsCollection.find({});
         const result = await cursor.toArray();
         res.send(result);
     })
+    
 
+    app.get('/featured-products/:id', async(req,res)=>{
+      try{
+        // const database = client.db("glowingCosmeticShop");
+        // const productsCollection = database.collection("featuredProductsCollection");
+        const id = req.params.id;
+        const product = await productsCollection.findOne({_id: id})
+        res.send(product)
+      } catch (error) {
+        console.error(error)
+        res.status(404).send('Product not found')
+      }
+    })
+
+
+    // review post
+
+    app.get('/review', async(req,res)=>{
+      const cursor =  reviewCollection.find({});
+        const result = await cursor.toArray();
+        res.send(result);
+
+    })
+
+
+    app.post('/review', async(req,res)=>{
+     const userReview = req.body
+     console.log(userReview);
+     const doc = {
+      name: userReview.name,
+      review: userReview.review,
+      rating: userReview.rating,
+      email: userReview.email,
+      photo: userReview.photo,
+      formattedDate : userReview.formattedDate
+     }
+     const result = await reviewCollection.insertOne(doc)
+     res.send(result)
+     
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
